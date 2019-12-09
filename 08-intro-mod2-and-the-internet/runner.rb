9b80dcb26e162702819fb789036ac174d5907858
Dwire <gjdwyer8@gmail.com>
2019-12-09 12:24:39 -0500

    done

---
+++ 08-intro-mod2-and-the-internet/runner.rb
@@ -0,0 +1,15 @@
+require 'sinatra'
+
+get '/' do 
+  '<h1>You have made it home 111819!!!</h1>'
+end 
+
+get '/profile' do 
+  '<h4>It is all about you it is your profile</h4>'
+end 
+
+# get '/profile/:variable' do 
+#   '<h4>It is all about you it is your profile</h4>'
+# end 
+
+
