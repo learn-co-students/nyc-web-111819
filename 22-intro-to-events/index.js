c5b39e137c39040e34ace1e771da4a823f62b411
Steven Doran <steven.doran@flatironschool.com>
2020-01-07 15:33:57 -0500

    post lecture

--- 22-intro-to-events/index.js
+++ 22-intro-to-events/index.js
@@ -1,22 +1,23 @@
 console.log("Movie Rating Application")
 
-function createLi() {
+function createLi(movie) {
   let li = document.createElement('li')
 
-  li.innerHTML = `
-    <h3>The Goonies</h3>
-    <img alt=""
-        src="https://images-na.ssl-images-amazon.com/images/I/515DYf99zfL.jpg" />
-    <h4>Year: </h4>
-    <p>1985</p>
-    <h4>Score: <span>0</span> </h4>
-    <button>Up Vote</button>
-    <button>Down Vote</button>
-  `
+  // li.innerHTML = `
+  //   <h3>${movie.title}</h3>
+  //   <img alt=""
+  //       src="${movie.imageUrl}" />
+  //   <h4>Year: </h4>
+  //   <p>${movie.year}5</p>
+  //   <h4>Score: <span>0</span> </h4>
+  //   <button class="up-vote">Up Vote</button>
+  //   <button>Down Vote</button>
+  // `
 
-  li.className = 'movie'
+  // li.className = 'movie'
 
-  return li
+  // return li
+  console.log(movie.title)
 }
 
 // ul.appendChild(li)
@@ -24,7 +25,24 @@ let ul = document.querySelector('ul')
 ul.append(createLi())
 
 
-let movies = [
+let upVotes = document.getElementsByClassName('up-vote')
+let upVotesArray = Array.from(upVotes)
+
+upVotesArray.forEach(function(button){
+  button.addEventListener('click', function(e){
+    let parentLi = e.target.parentNode
+    let span = parentLi.querySelector('span')
+    span.innerText = parseInt(span.innerText) + 1
+  })
+})
+
+
+const movies = [
+  {
+    title: 'The Goonies',
+    imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/515DYf99zfL.jpg',
+    year: 1985
+  },
   { 
     title: 'Free Willy',
     imageUrl: 'https://upload.wikimedia.org/wikipedia/en/thumb/b/b5/Free_willy.jpg/220px-Free_willy.jpg',
@@ -39,6 +57,9 @@ let movies = [
     title: 'Mean Girls',
     imageUrl: 'https://img01.mgo-images.com/image/thumbnail?id=1MV270609a1c6c89af5538a6d63cea71ed4&ql=70&sizes=310x465',
     year: 2004  
-  },
-  
+  }
 ]
+
+movies.forEach(function(movie){
+  return createLi(movie)
+})
